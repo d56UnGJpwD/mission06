cba03c308a495e1e730c9e0dd9e58cfdca0cddad
Kirill Brainard <braikiri@isu.edu>
2019-04-01 17:33:58 -0600

    classes created

---
+++ src/main/java/edu/isu/cs/cs3308/Driver.java
@@ -0,0 +1,10 @@
+package edu.isu.cs.cs3308;
+
+public class Driver
+{
+    public static void main()
+    {
+
+    }
+
+}
---
+++ src/main/java/edu/isu/cs/cs3308/structures/impl/Set.java
@@ -0,0 +1,54 @@
+package edu.isu.cs.cs3308.structures.impl;
+
+import java.util.Iterator;
+
+public class Set implements edu.isu.cs.cs3308.structures.Set
+{
+    @Override
+    public void add(Object e)
+    {
+
+    }
+
+    @Override
+    public void remove(Object e)
+    {
+
+    }
+
+    @Override
+    public boolean contains(Object e)
+    {
+        return false;
+    }
+
+    @Override
+    public Iterator iterator()
+    {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty()
+    {
+        return false;
+    }
+
+    @Override
+    public void addAll(edu.isu.cs.cs3308.structures.Set s)
+    {
+
+    }
+
+    @Override
+    public void retainAll(edu.isu.cs.cs3308.structures.Set s)
+    {
+
+    }
+
+    @Override
+    public void removeAll(edu.isu.cs.cs3308.structures.Set s)
+    {
+
+    }
+}
